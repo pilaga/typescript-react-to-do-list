6dd6c83aecb4212c0e0b4ea59dfe065cd9c53b3d
Pierre <pierre.lagadec@gmail.com>
2021-10-28 13:40:07 +1100

    adding type for list item props

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import TodoList from './components/TodoList';
 
 const App: React.FunctionComponent = () => {
+
+  const todos = [
+      {id: 't1', text: 'Empty dishwasher'},
+      {id: 't2', text: 'Call mom'},
+      {id: 't3', text: 'Renew car registration'},
+      {id: 't4', text: 'Walk the dog'}
+  ];
+
   return (
     <div className="App">
-      
+      <TodoList items={todos}/>
     </div>
   );
 }
